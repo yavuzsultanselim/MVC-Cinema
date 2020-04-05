60a97b648e840fd8fc732bd20097e38cbbbdb7be
yavuzsultanselim <47191389+yavuzsultanselim@users.noreply.github.com>
2020-04-05 19:44:04 +0300

    Proje eklendi

---
+++ WebCınemaMVC!!/WebCınemaMVC!!/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCınemaMVC__.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Vizyon()
+        {
+            return View();
+        }
+        public ViewResult Joker()
+        {
+            return View();
+        }
+
+    }
+}
